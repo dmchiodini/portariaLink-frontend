20b94e27ccc78a788bb554aab00c6e0a4ea4fe99
dmchiodini <g25_diego@hotmail.com>
2025-10-30 19:29:41 -0300

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { saveToken } from '@/lib/auth';
-import api from '@/lib/api';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { useForm } from 'react-hook-form';
-import { ErrorMessage } from "@hookform/error-message"
-import z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { saveToken } from "@/lib/auth";
+import api from "@/lib/api";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const loginSchema = z.object({
-  email: z.email('E-mail inválido').min(1, 'E-mail é obrigatório'),
-  password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
+  email: z.email("E-mail inválido").min(1, "E-mail é obrigatório"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
 });
 
 type LoginData = z.infer<typeof loginSchema>;
@@ -29,49 +33,77 @@ export default function Login() {
 
   const onSubmit = async (data: LoginData) => {
     try {
-      const res = await api.post('/auth/login', data);
+      const res = await api.post("/auth/login", data);
       saveToken(res.data.data.access_token);
-      router.push('/dashboard');
+      router.push("/dashboard");
     } catch (err: any) {
-      alert(err?.response?.data?.message || 'Erro ao fazer login');
+      alert(err?.response?.data?.message || "Erro ao fazer login");
     }
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-slate-200">
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded-xl shadow-md w-80">
-        <h1 className="text-2xl font-bold mb-4 text-center text-slate-800">PortariaLink</h1>
-        <div className='mb-4'>
-          <Input
-            defaultValue=""
-            type="email"
-            placeholder="E-mail"
-            {...register('email', { required: true })}
-          />
-          <span className='text-red-600 -mt-6 text-xs pl-2'>{errors?.email?.message}</span>
+    <main className="min-h-screen flex items-center justify-center bg-slate-300 p-4 md:p-none">
+      <div className="flex h-[400px] rounded-xl shadow-lg bg-white md:w-auto w-full">
+        <div className="md:flex items-center justify-center h-full w-80 hidden ">
+          <Image src={"/logo.png"} alt={""} width={250} height={250} />
         </div>
-
-        <div className='mb-4'>
-          <Input
-            defaultValue=""
-            type="password"
-            placeholder="Senha"
-            {...register('password', { required: true })}
+        <div className="md:bg-slate-700 bg-white w-full md:w-80 h-full flex flex-col items-center md:justify-center rounded-xl p-6 gap-4">
+          <Image
+            src={"/logo-mobile.png"}
+            alt={""}
+            width={200}
+            height={100}
+            className="md:hidden block"
           />
-          <span className='text-red-600 -mt-6 text-xs pl-2'>{errors?.password?.message}</span>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+            <div className="md:bg-slate-200  p-4 rounded-xl w-full">
+              <h1 className="text-2xl font-bold mb-4 text-center text-slate-700">
+                Bem vindo
+              </h1>
+              <div className="mb-4 w-full">
+                <Input
+                  defaultValue=""
+                  type="email"
+                  placeholder="E-mail"
+                  className="bg-white"
+                  {...register("email", { required: true })}
+                />
+                <span className="text-red-600 -mt-6 text-xs pl-2">
+                  {errors?.email?.message}
+                </span>
+              </div>
+
+              <div className="mb-2 w-full">
+                <Input
+                  defaultValue=""
+                  type="password"
+                  placeholder="Senha"
+                  className="bg-white"
+                  {...register("password", { required: true })}
+                />
+                <span className="text-red-600 -mt-6 text-xs pl-2">
+                  {errors?.password?.message}
+                </span>
+              </div>
+              <Link
+                href={"#"}
+                className="text-white md:text-slate-700 text-xs pl-1"
+              >
+                Esqueceu a senha?
+              </Link>
+
+              <Button
+                type="submit"
+                className="w-full bg-amber-600 hover:bg-amber-600/90 my-4"
+                aria-label="Submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Entrando..." : "Entrar"}
+              </Button>
+            </div>
+          </form>
         </div>
-
-
-        <Button
-          type="submit"
-          className="w-full "
-          aria-label="Submit"
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? 'Entrando...' : 'Entrar'}
-        </Button>
-      </form>
+      </div>
     </main>
   );
 }
-
