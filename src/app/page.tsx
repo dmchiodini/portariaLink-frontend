3218c1d126a27f4e2f3d224ce105ed94469043ba
Diego Chiodini <diegochiodini@pecege.com>
2025-11-04 12:05:59 -0300

    refactor: ajusta layout da tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { saveToken } from "@/lib/auth";
 import api from "@/lib/api";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
+import { useForm, FormProvider } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { User, Lock } from "lucide-react";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const loginSchema = z.object({
   email: z.email("E-mail inválido").min(1, "E-mail é obrigatório"),
@@ -23,13 +21,16 @@ type LoginData = z.infer<typeof loginSchema>;
 
 export default function Login() {
   const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<LoginData>({
+
+  const methods = useForm<LoginData>({
     resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
   });
+
+  const { handleSubmit, formState: { isSubmitting } } = methods;
 
   const onSubmit = async (data: LoginData) => {
     try {
@@ -42,66 +43,73 @@ export default function Login() {
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-slate-300 p-4 md:p-none">
+    <main className="min-h-screen flex items-center justify-center bg-background p-4 md:p-none">
+      <ThemeToggle />
       <div className="flex h-[400px] rounded-xl shadow-lg bg-white md:w-auto w-full">
-        <div className="md:flex items-center justify-center h-full w-80 hidden ">
-          <Image src={"/logo.png"} alt={""} width={250} height={250} />
-        </div>
-        <div className="md:bg-slate-700 bg-white w-full md:w-80 h-full flex flex-col items-center md:justify-center rounded-xl p-6 gap-4">
+        <div className="md:flex flex-col items-center justify-center h-full w-80 hidden ">
+          <Image src={"/condominium.png"} alt={""} width={250} height={250} />
           <Image
-            src={"/logo-mobile.png"}
+            src={"/logo-mobile1.png"}
+            alt={""}
+            width={200}
+            height={100}
+            className="-ml-2"
+          />
+        </div>
+        <div className="md:bg-foreground bg-white w-full md:w-80 h-full flex flex-col items-center md:justify-center rounded-xl p-6 gap-4">
+          <Image
+            src={"/logo-mobile1.png"}
             alt={""}
             width={200}
             height={100}
             className="md:hidden block"
           />
-          <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-            <div className="md:bg-slate-200  p-4 rounded-xl w-full">
-              <h1 className="text-2xl font-bold mb-4 text-center text-slate-700">
-                Bem vindo
-              </h1>
-              <div className="mb-4 w-full">
-                <Input
-                  defaultValue=""
-                  type="email"
-                  placeholder="E-mail"
-                  className="bg-white"
-                  {...register("email", { required: true })}
-                />
-                <span className="text-red-600 -mt-6 text-xs pl-2">
-                  {errors?.email?.message}
-                </span>
-              </div>
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+              <div className="bg-white p-4 rounded-xl w-full flex flex-col">
+                <h1 className="text-2xl font-bold mb-4 text-center text-foreground  dark:text-background">
+                  Bem vindo
+                </h1>
+                <div className="mb-4 w-full">
+                  <Input
+                    name="email"
+                    type="email"
+                    placeholder="Digite seu email"
+                    icon={User}
+                  />
+                </div>
 
-              <div className="mb-2 w-full">
-                <Input
-                  defaultValue=""
-                  type="password"
-                  placeholder="Senha"
-                  className="bg-white"
-                  {...register("password", { required: true })}
-                />
-                <span className="text-red-600 -mt-6 text-xs pl-2">
-                  {errors?.password?.message}
-                </span>
-              </div>
-              <Link
-                href={"#"}
-                className="text-white md:text-slate-700 text-xs pl-1"
-              >
-                Esqueceu a senha?
-              </Link>
+                <div className="mb-2 w-full">
+                  <Input
+                    name="password"
+                    type="password"
+                    placeholder="Crie sua senha"
+                    icon={Lock}
+                  />
+                </div>
 
-              <Button
-                type="submit"
-                className="w-full bg-amber-600 hover:bg-amber-600/90 my-4"
-                aria-label="Submit"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Entrando..." : "Entrar"}
-              </Button>
-            </div>
-          </form>
+                <div className="mb-2 pl-1">
+                  <Link
+                    href={"#"}
+                    className="dark:text-background text-xs"
+                  >
+                    Esqueceu a senha?
+                  </Link>
+                </div>
+
+                <div className="my-3 w-full border-">
+                  <Button
+                    type="submit"
+                    aria-label="Submit"
+                    disabled={isSubmitting}
+                    variant="default"
+                  >
+                    {isSubmitting ? "Entrando..." : "Entrar"}
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </FormProvider>
         </div>
       </div>
     </main>
